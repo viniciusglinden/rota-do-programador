cf6e8ae651649c3ef7e8da2f456f0bfea0d5049a
Vinícius Gabriel Linden <linden@viniciuslinden.xyz>
2023-07-20 21:16:32 +0200

    add subshell to build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@ cd "$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
 mkdir -p pdf/
 
 for dir in ./presentations/*; do
-    if [[ -d "${dir}" ]]; then
-        cd -- "${dir}"
-        pdflatex -shell-escape -- main.tex
-        rm -fr -- *.aux *.log *.nav *.out *.snm *.toc *.vrb _minted-main/
-        mv -- main.pdf "../../pdf/$(basename -- "${dir}").pdf"
-    fi
+    [[ -d "${dir}" ]] &&
+        (
+            cd -- "${dir}"
+            pdflatex -shell-escape -- main.tex
+            rm -fr -- *.aux *.log *.nav *.out *.snm *.toc *.vrb _minted-main/
+            mv -- main.pdf "../../pdf/$(basename -- "${dir}").pdf"
+        )
 done
